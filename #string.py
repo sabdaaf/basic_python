b46656c12d0215e7c3b6b5df7707e6b7148bade2
Fuad Ali Rahman <alirahmanfuad@gmail.com>
2024-10-08 23:46:27 +0700

    first commit

---
+++ #string.py
@@ -0,0 +1,22 @@
+#string
+a = "Hello, World!"
+print("jumlah karakter", len(a))
+print(a[1:8],"\n")
+#spasi tuh diitung yagesya
+
+
+for x in "banana ":
+  print(x) 
+
+txt = "The best things in life are free!"
+print("free" in txt)
+
+txt = "The best things in life are free!"
+if "free" in txt:
+  print("Yes, 'free' is present.")
+  
+txt = "The best things in life are free!"
+if "expensive" in txt:
+  print("'expensive' is  present.")
+else:
+  print("expensive is not present")
---
+++ variable.py
@@ -0,0 +1,20 @@
+#w3school variable
+
+#global variable
+x="awesome";
+
+def myfunc(): 
+  x = "fantastic"
+  print("x inside is "+ x)
+
+myfunc()
+
+print("x is outside " , x,"\n")
+
+#assign multiple value-unpack list
+fruits = ["apple", "banana", "cherry"]
+a, b, c = fruits
+
+print(a)
+print(b)
+print(c)
